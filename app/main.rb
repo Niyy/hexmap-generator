76234f1665c11f6ad17011d9ce65d84841c0bcf1
Austin Meyer <meyerforge@gmail.com>
2020-02-20 12:14:26 -0800

    Grid ready.

--- app/main.rb
+++ app/main.rb
@@ -39,33 +39,40 @@ $rads_to_degrees = 180 / Math::PI
 # end
 
 # $tile = Tile.new
-# def tick args
-#     $tile.outputs = args.outputs
-#     args.grid.origin_center!
-#     #gridSnap(args, $hex_outline)
+def tick args
+    offset = 32 / 2
+
+    for i in 0..52 do
+        for j in 0..21 do
+            args.outputs.sprites << [(i * 24), (j * 30) + 32 - offset, 32, 32, "sprites/hex_outline.png"]
+        end
+
+        if offset == 32 / 2
+            offset = 0
+        else
+            offset = 32 / 2
+        end
+    end
+
     
-#     for i in 0..5 do
-#         radians = (30 + (60 * i)) * $degrees_to_rads
-#         cos = Math.cos(radians)
-#         sin = Math.sin(radians)
-#         sprite_info = [cos * 31, sin * 31, 32, 32, "sprites/hex_grass.png"]
+    # for i in 0..5 do
+    #     radians = (30 + (60 * i)) * $degrees_to_rads
+    #     cos = Math.cos(radians)
+    #     sin = Math.sin(radians)
+    #     sprite_info = [cos * 31, sin * 31, 32, 32, "sprites/hex_grass.png"]
 
-#         if sin < 0
-#             sprite_info.y += 0.5
-#         elsif sin > 0
-#             sprite_info.y -= 0.9
-#         end
+    #     if sin < 0
+    #         sprite_info.y += 0.5
+    #     elsif sin > 0
+    #         sprite_info.y -= 0.9
+    #     end
 
-#         if cos < 0.1
-#             sprite_info.x += 0.9
-#         elsif cos > 0.1
-#             sprite_info.x -= 0.5
-#         end
+    #     if cos < 0.1
+    #         sprite_info.x += 0.9
+    #     elsif cos > 0.1
+    #         sprite_info.x -= 0.5
+    #     end
         
-#         args.outputs.sprites << sprite_info
-#     end
-
-#     $tile.tile_info = [0, 0, 32, 32, "sprites/hex_grass.png"]
-#     $tile.draw
-#     #args.outputs.sprites << $hex_outline
-# end
+    #     args.outputs.sprites << sprite_info
+    # end
+end
