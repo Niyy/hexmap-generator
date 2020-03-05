6f89857d25603befd4eb657b25e1f5cc3d46fdeb
Austin Meyer <meyerforge@gmail.com>
2020-03-05 10:44:36 -0800

    Work on fixing [] operator error.

--- app/main.rb
+++ app/main.rb
@@ -5,7 +5,6 @@ class Tile
     attr_accessor :position
     attr_gtk
 
-    @position
     @sprite
     @dimensions
 
@@ -116,57 +115,76 @@ class Continet
     @tiles
     @width
     @size
+    @created
 
     def initialize root, grid
-        rng = Random.new
-
         @root_tile = root
         @tiles = Hash.new
         @size = 6
+        @created = false;
+    end
 
-        root.getSprite.path = "sprites/hex_grass.png"
-        cur = root
+    def landCreation grid, grid_x, grid_y, queue
+        rng = Random.new
+
+        # if(rng.rand(1) < 0.8)
+        #     grid[[grid_x, grid_y]].getSprite.path = "sprites/circle-black.png"
+
+        #     added_land = [grid_x, grid_y]
+        #     queue.push(added_land)
+        # end
+    end
+
+    def continetCreator grid
+        queue = Array.new
+
+        @root_tile.getSprite.path = "sprites/hex_grass.png"
+        queue.push(@root_tile)
         tile_offset = 0
 
-        if root.getPosition.x % 2 == 0
-            tile_offset = 1
-        end
 
-        for i in 0...@size do
-            #roll = rng.rand(6)
+        while !queue.empty?
+            cur = queue.pop
 
-            case i
-            when 0
-                grid[[cur.position[0] - 1, cur.position[1] + 1 - tile_offset]].getSprite.path = "sprites/circle-black.png"
-            when 1
-                grid[[cur.position[0], cur.position[1] + 1]].getSprite.path = "sprites/circle-blue.png"
-            when 2
-                grid[[cur.position[0] + 1, cur.position[1] + 1 - tile_offset]].getSprite.path = "sprites/circle-gray.png"
-            when 3
-                grid[[cur.position[0] - 1, cur.position[1] - tile_offset]].getSprite.path = "sprites/circle-red.png"
-            when 4
-                grid[[cur.position[0], cur.position[1] - 1]].getSprite.path = "sprites/circle-yellow.png"
-            when 5
-                grid[[cur.position[0] + 1, cur.position[1] - tile_offset]].getSprite.path = "sprites/circle-violet.png"
+            if (cur.position[0] % 2) == 0
+                tile_offset = 1
+            else
+                tile_offset = 0
             end
+            
+            landCreation(grid, (cur.position[0] - 1), (cur.position[1] + 1 - tile_offset), queue)
+            # landCreation(grid, cur.position[0], cur.position[1] + 1, queue)
+            # landCreation(grid, cur.position[0] + 1, cur.position[1] + 1 - tile_offset, queue)
+            # landCreation(grid, cur.position[0] - 1, cur.position[1] - tile_offset, queue)
+            # landCreation(grid, cur.position[0], cur.position[1] - 1, queue)
+            # landCreation(grid, cur.position[0] + 1, cur.position[1] - tile_offset, queue)
         end
+
+        @created = true;
     end
+
+    def getCreated
+        return @created
+    end
+
 end
 
+
 $rng = Random.new
-root_x = $rng.rand(52)
-root_y = $rng.rand(21)
 $grid = HexGrid.new
 $continet
 
 def tick args
-    $grid.args = args
+    $grid.args = args   
     $grid.setUpGrid
     root_x ||= $rng.rand(52)
     root_y ||= $rng.rand(21)
 
-    $continet = Continet.new($grid.getHex(4, 4), $grid.getGrid)
-    $continet = Continet.new($grid.getHex(9, 4), $grid.getGrid)
+    $continet ||= Continet.new($grid.getHex(4, 4), $grid.getGrid)
+
+    if(!$continet.getCreated)
+        $continet.continetCreator $grid.getGrid
+    end
 
     $grid.draw
     $grid.input
