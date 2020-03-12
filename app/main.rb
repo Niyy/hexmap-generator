b01ec06506eb9fcddd6239e9ff05978a0ee9641e
Austin Meyer <meyerforge@gmail.com>
2020-03-11 18:38:21 -0700

    More continents spawn.

--- app/main.rb
+++ app/main.rb
@@ -80,8 +80,8 @@ class HexGrid
 end
 
 
-class Continet
-    attr_accessor :tile_queue, :initialized, :created
+class Continent
+    attr_accessor :tile_queue, :initialized, :created, :long_adder, :wide_adder
 
     @root_tile
     @tiles
@@ -96,20 +96,22 @@ class Continet
         @root_tile = root
         @tiles = Hash.new
         @rng = rng_gen
-        @size = 100
+        @size = 50
         @current_size = 0
         @tile_offset = 0
+        @long_adder = 0
+        @wide_adder = 0
         @created = false
         @initialized = false
         @tile_queue = Array.new
     end
 
-    def spawnTile grid, grid_x, grid_y, queue
+    def spawnTile grid, grid_x, grid_y, queue, current
 
         if(grid_x >= 0 && grid_x < 53 && grid_y >= 0 && grid_y < 22)
 
             rand_num = @rng.rand(@size)
-            dist = (Math.sqrt( (@root_tile.position[0] - grid_x)**2 + (@root_tile.position.y - grid_y)**2) * (@size / 10))
+            dist = (Math.sqrt( (current.position[0] - grid_x)**2 + (current.position.y - grid_y)**2) * (@size / 5))
 
             if(rand_num > dist && !grid[[grid_x, grid_y]].tiled)
                 grid[[grid_x, grid_y]].sprite.path = "sprites/hex_grass.png"
@@ -134,22 +136,23 @@ class Continet
 
     def addLand grid
         if !@tile_queue.empty?
-
-            cur = @tile_queue.shift
+            current_root ||= @root_tile
+            current_tile = @tile_queue.shift
+            checkForOffShoot current_tile, current_root
             @current_size += 1
 
-            if (cur.position[0] % 2) == 0
+            if (current_tile.position[0] % 2) == 0
                 @tile_offset = 1
             else
                 @tile_offset = 0
             end
             
-            spawnTile(grid, (cur.position[0] - 1), (cur.position[1] + 1 - @tile_offset), @tile_queue)
-            spawnTile(grid, cur.position[0], cur.position[1] + 1,  @tile_queue)
-            spawnTile(grid, cur.position[0] + 1, cur.position[1] + 1 - @tile_offset, @tile_queue)
-            spawnTile(grid, cur.position[0] - 1, cur.position[1] - @tile_offset,  @tile_queue)
-            spawnTile(grid, cur.position[0], cur.position[1] - 1,  @tile_queue)
-            spawnTile(grid, cur.position[0] + 1, cur.position[1] - @tile_offset, @tile_queue)
+            spawnTile(grid, (current_tile.position[0] - 1), (current_tile.position[1] + 1 - @tile_offset), @tile_queue, current_root)
+            spawnTile(grid, current_tile.position[0], current_tile.position[1] + 1,  @tile_queue, current_root)
+            spawnTile(grid, current_tile.position[0] + 1, current_tile.position[1] + 1 - @tile_offset, @tile_queue, current_root)
+            spawnTile(grid, current_tile.position[0] - 1, current_tile.position[1] - @tile_offset,  @tile_queue, current_root)
+            spawnTile(grid, current_tile.position[0], current_tile.position[1] - 1,  @tile_queue, current_root)
+            spawnTile(grid, current_tile.position[0] + 1, current_tile.position[1] - @tile_offset, @tile_queue, current_root)
 
             if(@current_size >= @size)
                 @tile_queue.clear()
@@ -157,44 +160,46 @@ class Continet
             end
         end
     end
+
+    def checkForOffShoot current_tile, current_root
+        rand_num = @rng.rand(100)
+
+        if(rand_num >= 0.90)
+            current_root.sprite.path = "sprites/hex_grass.png"
+            current_root = current_tile
+            current_tile.sprite.path = "sprites/circle-blue.png"
+            puts "Changed root too #{current_root.position}"
+        end
+    end
 end
 
 
 $rng = Random.new
 $grid = HexGrid.new
-$continentOne
-$continentTwo
+$continents
 
 def tick args
     $grid.args = args   
     $grid.setUpGrid
-    root_x ||= $rng.rand(52)
-    root_y ||= $rng.rand(21)
 
-    $continentOne ||= Continet.new($grid.grid_positions[[root_x, root_y]], $rng, $grid.grid_positions)
+    if($continents.nil?)
+        $continents ||= Array.new()
 
-    if(!$continentOne.initialized)
-        $continentOne.createContinent
+        for i in 1..5 do
+            root_x = $rng.rand(52)
+            root_y = $rng.rand(21)
 
-        root_x = $rng.rand(52)
-        root_y = $rng.rand(21)
-
-        puts "Did continentOne creation"
-    end
-    if(!$continentOne.created)
-        $continentOne.addLand $grid.grid_positions
+            $continents.push Continent.new($grid.grid_positions[[root_x, root_y]], $rng, $grid.grid_positions)
+            $continents.last.createContinent
+        end
     end
 
-
-    $continentTwo ||= Continet.new($grid.grid_positions[[root_x, root_y]], $rng, $grid.grid_positions)
-
-    if(!$continentTwo.initialized)
-        root_x = $rng.rand(52)
-        root_y = $rng.rand(21)
-
-        $continentTwo.createContinent
-    elsif(!$continentTwo.created)
-        $continentTwo.addLand $grid.grid_positions
+    for i in 0..4 do
+        puts "huhuh"
+        if(!$continents[i].created)
+            $continents[i].addLand $grid.grid_positions
+        end
+        puts "haha"
     end
 
     $grid.draw
